df345b63692d2cce2938090a35fdad34470186e4
leotte_cd <leonardo.leotte@creativedrive.com>
2021-02-05 00:07:39 -0300

    moved UI and hooks to new Manager MonoBehaviour, that is injected into the scene and never unloads.

--- TrainingMode.cs
+++ TrainingMode.cs
@@ -1,56 +1,29 @@
 ﻿using Modding;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace TrainingMode
 {
     public class TrainingMode : Mod
-    {
+	{
 		#region Variables
-		GameObject _trainingCanvas;
-		Text _titleLabel;
+		GameObject _trainingModeManager;
 		#endregion
 
 		#region Initialization
 		public override string GetVersion()
 		{
-			return "0.0.0.1";
+			return string.Format("0.0.0.1 - {0}", System.DateTime.Now.ToShortTimeString());
 		}
 
 		public override void Initialize()
 		{
 			Log("TrainingMode started!");
 
+			//inject our friendly manager into the scene
+			_trainingModeManager = new GameObject("TrainingModeManager", typeof(TrainingModeManager));
+
 			//register events
-			GameManager.instance.OnFinishedEnteringScene += OnSceneEnter;
-			ModHooks.Instance.HeroUpdateHook += OnHeroUpdated;
-		}
-
-		private void InitializeUI()
-		{
-			if (_trainingCanvas == null)
-				_trainingCanvas = CanvasUtil.CreateCanvas(RenderMode.ScreenSpaceOverlay, 1);
-
-			if (_titleLabel == null)
-				_titleLabel = CanvasUtil.CreateTextPanel(_trainingCanvas.gameObject, "TrainingMode - Runtime", 12, TextAnchor.UpperRight, 
-					new CanvasUtil.RectData(new Vector2(200, 100), new Vector2(100, 100)))
-					.GetComponentInChildren<Text>();
-		}
-		#endregion
-
-		#region Event Methods
-		private void OnSceneEnter()
-		{
-			Log("SceneEntered");
-			InitializeUI();
-		}
-
-		private void OnHeroUpdated()
-		{
-			var heroState = HeroController.instance.CanSuperDash();
-			if (_titleLabel != null)
-				_titleLabel.text = string.Format("CanSuperDash: {0}", heroState);
-			//Log(string.Format("Hero state: {0}", heroState));
+			ModHooks.Instance.HeroUpdateHook += _trainingModeManager.GetComponent<TrainingModeManager>().OnHeroUpdated;
 		}
 		#endregion
 	}
---
+++ TrainingModeManager.cs
@@ -0,0 +1,88 @@
+﻿using Modding;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TrainingMode
+{
+	public class TrainingModeManager : MonoBehaviour
+	{
+		#region Variables
+		GameObject _trainingCanvas;
+		Text _titleLabel;
+		#endregion
+
+		#region Unity Methods
+		void Awake()
+		{
+			DontDestroyOnLoad(gameObject);
+			UnityEngine.SceneManagement.SceneManager.activeSceneChanged += OnSceneChanged;
+		}
+
+		void Start()
+		{
+			Modding.Logger.Log("NEW BEHAVIOUR ACHIEVED");
+		}
+
+		void FixedUpdate()
+		{
+			//Modding.Logger.Log(string.Format("Fixed Update tick - {0}", Time.frameCount));
+		}
+		#endregion
+
+		private void InitializeUI()
+		{
+			if (_trainingCanvas?.gameObject == null)
+			{
+				_trainingCanvas = CanvasUtil.CreateCanvas(RenderMode.ScreenSpaceOverlay, 1);
+				//hold it under our wings to survive scene loads
+				_trainingCanvas.transform.SetParent(gameObject.transform);
+				_trainingCanvas.name = "TrainingCanvas";
+			}
+
+			Modding.Logger.Log(string.Format("Canvas active: {0}", _trainingCanvas.gameObject.activeInHierarchy));
+
+			_trainingCanvas.SetActive(true);
+
+			if (_titleLabel?.gameObject == null)
+				_titleLabel = CanvasUtil.CreateTextPanel(_trainingCanvas.gameObject, "TrainingMode - Runtime", 12, TextAnchor.UpperRight,
+					new CanvasUtil.RectData(new Vector2(200, 100), new Vector2(100, 100)))
+					.GetComponentInChildren<Text>();
+
+			_titleLabel.gameObject.SetActive(true);
+		}
+
+		private IEnumerator WaitForSceneChangedFrame(Action onFrameWait)
+		{
+			yield return new WaitForFixedUpdate();
+			onFrameWait.Invoke();
+		}
+
+
+
+		#region Event Methods
+		public void OnHeroUpdated()
+		{
+			var heroState = HeroController.instance.CanSuperDash();
+			if (_titleLabel != null)
+				_titleLabel.text = string.Format("CanSuperDash: {0}", heroState);
+			//Modding.Logger.Log(string.Format("Hero state: {0}", heroState));
+		}
+
+		public void OnSceneChanged(UnityEngine.SceneManagement.Scene oldScene, UnityEngine.SceneManagement.Scene newScene)
+		{
+			//shitty callback for thread
+			StartCoroutine(WaitForSceneChangedFrame(OnFrameWait));
+
+			void OnFrameWait()
+			{
+				InitializeUI();
+			}
+		}
+		#endregion
+	}
+}
